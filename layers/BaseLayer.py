964802ffdbbb30933760575dc999b7cfb8bc4545
Gautier <gautier.blandin@gmail.com>
2022-07-18 23:07:31 +0200

    Add backward pass method to baselayer class

--- layers/BaseLayer.py
+++ layers/BaseLayer.py
@@ -6,7 +6,16 @@ class BaseLayer(ABC):
     def __init__(self, input_shape, output_shape):
         self.input_shape = input_shape
         self.output_shape = output_shape
+        self.eval_mode = False
 
     @abstractmethod
     def forward(self, input_data: np.ndarray) -> np.ndarray:
         pass
+
+    @abstractmethod
+    def backward(self, next_layer_gradient: np.ndarray) -> np.ndarray:
+        pass
+
+    def eval(self):
+        self.eval_mode = True
+
