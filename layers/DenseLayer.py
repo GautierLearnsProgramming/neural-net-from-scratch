9349592e66997fabc1e2ad7f395f82aac0199194
Gautier <gautier.blandin@gmail.com>
2022-07-18 23:08:33 +0200

    Update forward pass method of DenseLayer

--- layers/DenseLayer.py
+++ layers/DenseLayer.py
@@ -7,7 +7,7 @@ from activation.relu import relu
 class DenseLayer(BaseLayer):
     def __init__(self, input_shape, output_shape, activation_function: Callable[[float], float] = None):
         super().__init__(input_shape, output_shape)
-        self.input_data = None
+        self.output = None
         self.weights = np.random.randn(input_shape, output_shape)
         self.bias = np.random.randn(output_shape)
         if activation_function is None:
@@ -16,8 +16,13 @@ class DenseLayer(BaseLayer):
             self.activation_function = np.vectorize(activation_function)
 
     def forward(self, input_data: np.ndarray) -> np.ndarray:
-        self.input_data = input_data
-        return self.activation_function(np.dot(input_data, self.weights) + self.bias)
+        output = self.activation_function(np.dot(input_data, self.weights) + self.bias)
+        if not self.eval_mode:
+            self.output = output
+        return output
+
+    def backward(self, next_layer_gradient: np.ndarray) -> np.ndarray:
+        pass
 
     def load_weights(self, weights: np.ndarray, bias: np.ndarray):
         self.weights = weights
