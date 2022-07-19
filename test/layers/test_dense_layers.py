811b78deb33ac84e0754aff2f5dab5ccd6c0372c
Gautier <gautier.blandin@gmail.com>
2022-07-19 15:00:50 +0200

    Update tests for dense layer forward pass

--- test/layers/test_dense_layers.py
+++ test/layers/test_dense_layers.py
@@ -3,11 +3,10 @@ import numpy as np
 
 
 def test_dense_layer_forward():
-    dense_layer = DenseLayer(input_shape=2, output_shape=4)
-    weights = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
-    bias = np.array([100, 200, 300, 400])
+    dense_layer = DenseLayer(input_size=2, output_size=4)
+    weights = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [100, 200, 300, 400]])
 
-    dense_layer.load_weights(weights, bias)
+    dense_layer.load_weights(weights)
     input_data = np.array([[1, 2], [3, 4], [5, 6]])
     assert np.array_equal(dense_layer.forward(input_data), np.array([[111, 214, 317, 420], [123, 230, 337, 444],
                                                                      [135, 246, 357, 468]]))
