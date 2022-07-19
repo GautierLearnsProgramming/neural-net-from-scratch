bafc3e7a26d5ba9ce7d3a59e001a4e110fa91a7d
Gautier <gautier.blandin@gmail.com>
2022-07-19 19:05:27 +0200

    Add se_gradient computation function documentation

--- cost/se.py
+++ cost/se.py
@@ -6,4 +6,13 @@ def squared_error(z: np.ndarray, y: np.ndarray) -> np.ndarray:
 
 
 def compute_se_gradient(z: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """ Compute the gradient of the squared error cost function.
+
+    Args:
+        z: the predicted vector (ndarray of shape (batch_size, n))
+        y: the true vector (ndarray of shape (batch_size, n))
+    Returns:
+        The gradient of the squared error cost function with respect to the predicted vector
+        (ndarray of shape (batch_size, n))
+    """
     return 2 * (z - y)
