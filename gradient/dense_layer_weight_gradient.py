8998b7f19e3e68d57d2d79a0c5282f1fc081031b
Gautier <gautier.blandin@gmail.com>
2022-07-19 14:57:51 +0200

    Fix compute_layer_weight_gradient_batch

--- gradient/dense_layer_weight_gradient.py
+++ gradient/dense_layer_weight_gradient.py
@@ -24,8 +24,9 @@ def compute_dense_layer_gradient(y: np.ndarray, w: np.ndarray, next_layer_gradie
     return dJdw
 
 
-def compute_dense_layer_weight_gradient_batch(y: np.ndarray, w: np.ndarray, next_layer_gradient: np.ndarray,
-                                              activation_function_dif: Callable[[float], float]) -> np.ndarray:
+def compute_dense_layer_weight_gradient_batch(y: np.ndarray, next_layer_gradient: np.ndarray, w: np.ndarray,
+                                              activation_function_dif: Callable[[float], float],
+                                              verbose: int = 0) -> np.ndarray:
     """ Compute the gradient of the loss function with respect to the weights of the dense layer.
 
     This function compute the gradient relative to the weights of the dense layer k, connecting the k-1 layer to the k
@@ -37,14 +38,29 @@ def compute_dense_layer_weight_gradient_batch(y: np.ndarray, w: np.ndarray, next
         next_layer_gradient: The gradient of the loss function with respect to the output of the k layer (ndarray of
             shape (batch_size, m))
         activation_function_dif: The derivative of the activation function of the k layer.
+        verbose: Verbosity level
     Returns:
         The gradient of the loss function with respect to the weights of the k-1 layer (ndarray of shape (n, m))
     """
     h = np.vectorize(activation_function_dif)
-    dzdw = (h(y @ w).T @ y)  # (m, n)
 
     batch_size = y.shape[0]
     m = w.shape[1]
+    n = y.shape[1]
 
-    dJdw = dzdw.T * next_layer_gradient.reshape((batch_size, 1, m))  # (batch_size, n, m)
+    y = y.reshape((batch_size, 1, n))
+
+    dzdw = (h(y @ w).transpose(0, 2, 1) @ y)  # (m, n)
+
+    dJdw = dzdw.transpose(0, 2, 1) * next_layer_gradient.reshape((batch_size, 1, m))  # (batch_size, n, m)
+
+    if verbose > 0:
+        print()
+        print(f'y: \n{y}')
+        print(f'next_layer_gradient: \n{next_layer_gradient}')
+        print(f'w: \n{w}')
+        print(f'h(y @ w).T: \n{h(y @ w).transpose(0, 2, 1)}')
+        print(f'h(y @ w).T @ y: \n{h(y @ w).transpose(0, 2, 1) @ y}')
+        print(f'dJdw: \n{dJdw}')
+
     return dJdw.sum(axis=0)  # (n, m)
